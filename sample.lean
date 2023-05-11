7ba1d0bf6ab17d70eece94e3e784c7ecfd6f089b
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-05-12 06:13:35 +0900

    add: First commit

---
+++ sample.lean
@@ -0,0 +1,33 @@
+constant m: nat -- m is a natural number
+constant n: nat
+constants b1 b2 : bool -- declare two constants at once
+
+#check m
+#check n
+#check n + 0
+#check m * (n + 0)
+#check b1
+#check b1 && b2
+#check b1 || b2
+#check tt
+
+constant f: nat → nat
+constant f': nat → nat
+constant p : nat × nat
+constant q : prod nat nat
+constant g : nat → nat → nat
+constant g' : nat → (nat → nat)
+constant h : nat × nat → nat
+
+constant F: (nat → nat) → nat
+
+#check f
+#check f n
+#check g m n
+#check g m
+#check (m, n)
+#check p.1
+#check p.2
+#check (m, n).1
+#check (p.1, n)
+#check F f
