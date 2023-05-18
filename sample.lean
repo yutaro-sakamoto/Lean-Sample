15363ce7c0c16f1e71c091ed990f4bf6d750eae1
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-05-19 00:55:13 +0900

    update: Section 3.1

--- sample.lean
+++ sample.lean
@@ -331,3 +331,28 @@ namespace Section2_9
     #check @id β b
   end sub4
 end Section2_9
+
+namespace Section3_1
+  constant and : Prop → Prop → Prop
+  constant or : Prop → Prop → Prop
+  constant not : Prop → Prop
+  constant implies : Prop → Prop → Prop
+
+  variables p q r : Prop
+  #check and p q
+  #check or (and p q) r
+  #check implies (and p q) (and q p)
+
+  constant Proof : Prop → Type
+
+  constant and_comm : Π p q : Prop,
+    Proof (implies (and p q) (and q p))
+  
+  #check and_comm p q
+
+  constant modus_ponens : Π p q : Prop,
+    Proof (implies p q) → Proof p → Proof q
+
+  constant implies_intro :
+    Π p q : Prop, (Proof p → Proof q) → Proof (implies p q)
+end Section3_1
