39db80b873f8ca7e9febf6a236c2a4d2e832e48b
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-05-28 20:31:17 +0900

    update: Section 5.3

--- my_project/src/sample.lean
+++ my_project/src/sample.lean
@@ -438,4 +438,97 @@ example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
     generalize h : 3 = x,
     rw ←h
   end
+
+  example : 2 + 3 = 5 :=
+  begin
+    reflexivity
+  end
 end Section5_2
+
+namespace Section5_3
+  example (p q : Prop) : p ∨ q → q ∨ p :=
+  begin
+    intro h,
+    cases h with hp hq,
+    -- case hp : p
+    right, exact hp,
+    -- case hq : q
+    left, exact hq
+  end
+
+  example (p q : Prop) : p ∧ q → q ∧ p :=
+  begin
+    intro h,
+    cases h with hp hq,
+    constructor, exact hq, exact hp
+  end
+
+  example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
+  begin
+    apply iff.intro,
+    intro h,
+      cases h with hp hqr,
+      cases hqr with hq hr,
+        left, constructor, repeat { assumption },
+        right, constructor, repeat { assumption },
+    intro h,
+      cases h with hpq hpr,
+        cases hpq with hp hq,
+          constructor, exact hp, left, exact hq,
+        cases hpr with hp hr,
+          constructor, exact hp, right, exact hr
+  end
+
+  example (p q : ℕ → Prop) : (∃ x, p x) → ∃ x, p x ∨ q x :=
+  begin
+    intro h,
+    cases h with x px,
+    constructor, left, exact px,
+  end
+  example (p q : ℕ → Prop) : (∃ x, p x) → ∃ x, p x ∨ q x :=
+  begin
+    intro h,
+    cases h with x px,
+    existsi x, left, exact px,
+  end
+
+  example (p q : ℕ → Prop) :
+    (∃ x, p x ∧ q x) → ∃ x, q x ∧ p x :=
+  begin
+    intro h,
+    cases h with x hpq,
+    cases hpq with hp hq,
+    existsi x,
+    split; assumption
+  end
+
+  universes u v
+
+  def swap_pair {α : Type u} {β : Type v} : α × β → β × α :=
+  begin
+    intro p,
+    cases p with ha hb,
+    constructor, exact hb, exact ha
+  end
+
+  def swap_sum {α : Type u} {β : Type v} : α ⊕ β → β ⊕ α :=
+  begin
+    intro p,
+    cases p with ha hb,
+      right, exact ha,
+      left, exact hb
+  end
+
+  open nat
+
+  example (P : ℕ → Prop) (h₀ : P 0) (h₁ : ∀ n, P (succ n)) (m : ℕ) :
+    P m :=
+  begin
+    cases m with m', exact h₀, exact h₁ m'
+  end
+
+  example (p q : Prop) : p ∧ ¬ p → q :=
+  begin
+    intro h, cases h, contradiction
+  end
+end Section5_3
