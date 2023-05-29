2fff49130a09f556977148006530a48b8f6e196e
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-05-30 00:36:10 +0900

    update: Section 5.5

--- my_project/src/sample.lean
+++ my_project/src/sample.lean
@@ -758,3 +758,72 @@ namespace Section5_4
         exact ⟨hp, hq⟩ }
   end
 end Section5_4
+
+namespace Section5_5
+  example (p q : Prop) (hp : p) : p ∨ q :=
+  begin left, assumption end
+
+  example (p q : Prop) (hp : p) : p ∨ q :=
+  by { left, assumption }
+
+  example (p q : Prop) (hp : p) (hq : q) : p ∧ q :=
+  by split; assumption
+
+  example (p q : Prop) (hp : p) : p ∨ q :=
+  by { left, assumption } <|> { right, assumption }
+
+  example (p q : Prop) (hq : q) : p ∨ q :=
+  by { left, assumption } <|> { right, assumption }
+
+  example (p q r : Prop) (hp : p) : p ∨ q ∨ r :=
+  by repeat { {left, assumption} <|> right <|> assumption }
+
+  example (p q r : Prop) (hq : q) : p ∨ q ∨ r :=
+  by repeat { {left, assumption} <|> right <|> assumption }
+
+  example (p q r : Prop) (hr : r) : p ∨ q ∨ r :=
+  by repeat { {left, assumption} <|> right <|> assumption }
+
+  meta def my_tac : tactic unit :=
+  `[ repeat { {left, assumption} <|> right <|> assumption } ]
+
+  example (p q r : Prop) (hp : p) : p ∨ q ∨ r :=
+  by my_tac
+
+  example (p q r : Prop) (hq : q) : p ∨ q ∨ r :=
+  by my_tac
+
+  example (p q r : Prop) (hr : r) : p ∨ q ∨ r :=
+  by my_tac
+
+  example (p q r : Prop) (hp : p) (hq : q) (hr : r) :
+  p ∧ q ∧ r :=
+  by split; try {split}; assumption
+
+  example (p q r : Prop) (hp : p) (hq : q) (hr : r) :
+  p ∧ q ∧ r :=
+  begin
+  split,
+  all_goals { try {split} },
+  all_goals { assumption }
+  end
+
+  example (p q r : Prop) (hp : p) (hq : q) (hr : r) :
+  p ∧ q ∧ r :=
+  begin
+    split,
+    any_goals { split },
+    any_goals { assumption }
+  end
+
+  example (p q r : Prop) (hp : p) (hq : q) (hr : r) :
+    p ∧ ((p ∧ q) ∧ r) ∧ (q ∧ r ∧ p) :=
+  begin
+    repeat {any_goals {split}},
+    all_goals { assumption }
+  end
+
+  example (p q r : Prop) (hp : p) (hq : q) (hr : r) :
+    p ∧ ((p ∧ q) ∧ r) ∧ (q ∧ r ∧ p) :=
+  by repeat { any_goals { split <|> assumption }}
+end Section5_5
