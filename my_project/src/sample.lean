09a6471f394afc88cecbb76bd41d5096ed4e60d6
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-06-02 03:32:46 +0900

    update: Sectoin 5.7

--- my_project/src/sample.lean
+++ my_project/src/sample.lean
@@ -962,9 +962,31 @@ namespace Section5_7_3
 
   theorem reverse_mk_symm (xs : list α) :
     reverse (mk_symm xs) = mk_symm xs :=
-  by { unfold mk_symm, simp }
-
-  theorem reverse_mk_symm (xs : list α) :
-    reverse (mk_symm xs) = mk_symm xs :=
   by simp [mk_symm]
+  
+  section
+  local attribute [simp] reverse_mk_symm
+
+  example (xs ys : list ℕ) : 
+    reverse (xs ++ mk_symm ys) = mk_symm ys ++ reverse xs :=
+  by simp
+
+  example (xs ys : list ℕ) (p :list ℕ → Prop)
+      (h : p (reverse (xs ++ (mk_symm ys)))) :
+    p (mk_symm ys ++ reverse xs) :=
+  by simp at h; assumption
+  end
+
+  run_cmd mk_simp_attr `my_simps
+
+  attribute [my_simps] reverse_mk_symm
+
+  example (xs ys : list ℕ) :
+    reverse (xs ++ mk_symm ys) = mk_symm ys ++ reverse xs :=
+  by simp with my_simps
+
+  example (xs ys : list ℕ) (p : list ℕ → Prop)
+    (h : p (reverse (xs ++ (mk_symm ys)))) :
+      p (mk_symm ys ++ reverse xs) :=
+  by simp with my_simps at h; assumption
 end Section5_7_3
