e9d856cc4555ce4e980d0a3720a03578c868737b
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-06-09 05:55:10 +0900

    update: Section 6.11

--- my_project/src/section6.lean
+++ my_project/src/section6.lean
@@ -2,6 +2,7 @@ import data.nat.basic
 import algebra.group.basic
 import data.int.basic
 import algebra.ring.basic
+import algebra.order.monoid.lemmas
 
 section
 variables x y : ℕ
@@ -319,3 +320,44 @@ namespace Section6_9
   set_option pp.beta true
   #check (λ x, x + 1) 1
 end Section6_9
+
+namespace Section6_11
+  open nat
+
+  #check succ_ne_zero
+  #check @mul_zero
+  #check @mul_one
+  #check @sub_add_eq_add_sub
+  #check @le_iff_lt_or_eq
+
+  #check @neg_neg
+  #check pred_succ
+
+  #check @nat.lt_of_succ_le
+  #check @lt_of_not_ge
+  #check @lt_of_le_of_ne
+  #check @add_lt_add_of_lt_of_le
+
+  #check @add_le_add_left
+  #check @add_le_add_right
+
+  #check mul_inv_self
+  #check neg_add_self
+
+  #check @prod.mk
+  #check @prod.fst
+  #check @prod.snd
+  #check @prod.rec
+
+  #check @and.intro
+  #check @and.elim
+  #check @and.left
+  #check @and.right
+  #check @or.inl
+  #check @or.inr
+  #check @or.elim
+  #check @exists.intro
+  #check @exists.elim
+  #check @eq.refl
+  #check @eq.subst
+end Section6_11
