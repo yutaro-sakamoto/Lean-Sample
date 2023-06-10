97b406854576575589a82b6c9dbd33739c40faca
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-06-10 21:11:02 +0900

    update: Section 7_1

--- my_project/src/section6.lean
+++ my_project/src/section6.lean
@@ -361,3 +361,75 @@ namespace Section6_11
   #check @eq.refl
   #check @eq.subst
 end Section6_11
+
+namespace Section7_1
+  inductive weekday : Type
+  | sunday : weekday
+  | monday : weekday
+  | tuesday : weekday
+  | wednesday : weekday
+  | thursday : weekday
+  | friday : weekday
+  | saturday : weekday
+
+  #check weekday.sunday
+  #check weekday.wednesday
+  open weekday
+  #check friday
+
+  def number_of_day(d: weekday) : ℕ :=
+  weekday.rec_on d 1 2 3 4 5 6 7
+
+  #reduce number_of_day weekday.sunday
+
+  namespace weekday
+  @[reducible]
+  private def cases_on := @weekday.cases_on
+  def number_of_day (d: weekday) : nat :=
+  cases_on d 1 2 3 4 5 6 7
+  end weekday
+
+  #reduce weekday.number_of_day weekday.sunday
+  open weekday (renaming cases_on → cases_on)
+  #reduce number_of_day
+  #check cases_on
+
+  namespace weekday
+    def next (d: weekday) : weekday :=
+    weekday.cases_on d monday tuesday wednesday thursday friday saturday sunday
+
+    def previous (d: weekday) : weekday :=
+    weekday.cases_on d saturday sunday monday tuesday wednesday thursday friday
+
+    #reduce next (next tuesday)
+    #reduce next (previous tuesday)
+
+    example : next (previous tuesday) = tuesday := rfl
+
+    theorem next_previous (d: weekday) :
+      next (previous d) = d :=
+    weekday.cases_on d
+      (show next (previous sunday) = sunday, from rfl)
+      (show next (previous monday) = monday, from rfl)
+      (show next (previous tuesday) = tuesday, from rfl)
+      (show next (previous wednesday) = wednesday, from rfl)
+      (show next (previous thursday) = thursday, from rfl)
+      (show next (previous friday) = friday, from rfl)
+      (show next (previous saturday) = saturday, from rfl)
+    
+    theorem next_previous2 (d : weekday) :
+      next (previous d) = d :=
+    weekday.cases_on d rfl rfl rfl rfl rfl rfl rfl
+
+    theorem next_previous3 (d : weekday) :
+      next (previous d) = d :=
+    by apply weekday.cases_on d; refl
+
+    theorem next_previous4 (d : weekday) :
+      next (previous d) = d :=
+    by apply weekday.rec_on d; refl
+  end weekday
+
+  def band (b1 b2 : bool) : bool :=
+  bool.cases_on b1 ff b2
+end Section7_1
